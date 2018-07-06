11402ea51c0209a625d3a2c6c5140bb3d304560b
awasthishubh <aw.shubh@gmail.com>
2018-07-07 00:08:45 +0530

    Load data from cookie if logged in

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,11 +1,33 @@
 import React, {Component} from 'react'
-import { relative, isAbsolute } from 'path';
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
+import {updateDashboardData, del} from '../actions/dashboard_action'
+
 import UpperLeft from './dashboard/dashUpperLeft'
 import UpperRight from './dashboard/dashUpperRight'
 import BottomLeft from './dashboard/dashBottomLeft'
 import BottomRight from './dashboard/dashBottomRight'
 
-export default class extends Component{
+function Loader(props){
+    if(!props.loggedIn)
+    return(
+        <div style={{position: 'fixed', top:0, bottom:0,left:0, right:0, zIndex:1000, background: 'rgba(0,0,0, 0.7)' }}>
+        <div class="preloader-wrapper big active" style={{height:200, width:200, position: 'absolute', top:'50%', left:'50%', margin: -100}}>
+            <div class="spinner-layer spinner-green-only">
+            <div class="circle-clipper  left">
+                <div class="circle"></div>
+            </div><div class="gap-patch">
+                <div class="circle"></div>
+            </div><div class="circle-clipper right">
+                <div class="circle"></div>
+            </div>
+            </div>
+        </div>
+       </div>
+    )
+    return <div/>
+}
+export class dashboard extends Component{
     constructor(props){
         super(props)
         this.sideNav=React.createRef();
@@ -14,16 +36,31 @@ export default class extends Component{
         // M.Sidenav.init(this.sideNav.current)
         var elems = document.querySelectorAll('.sidenav');
         window.sideInstance = M.Sidenav.init(elems[0]);
+        this.props.updateDashboardData('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2lkIjoiYWNtIn0.QVu651zEd4hLJf6Bc6NHoVn7Y4Vrj3kNj5XGBJMY6Xo')
         // instances.open();
     }
     render(){
+        console.log(this.props)
         return(
             <div>
            <UpperLeft/>
-           <BottomLeft/>
+           <BottomLeft mems={this.props.dashMembers}/>
            <UpperRight/>
            <BottomRight/>
-            </div>
+           <Loader loggedIn={this.props.isLoggedIn} del={this.props.del}/>
+           </div>
+
+
         )
     }
 }
+
+function mapStateToProps(state){
+    return state
+}
+
+function mapDispatchToProps(dispatch){
+    return bindActionCreators({updateDashboardData, del}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(dashboard)
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,13 @@
 import { combineReducers } from 'redux';
 import MemDetails from './memDetails_reducer'
-import {all, members, details, token  } from './organisations_reducer'
+import {all, members, details, token, isLoggedIn} from './dashboard_reducer'
 import OrgReg from './orgReg_reducer'
 import {usidIsAvailable} from './orgReg_reducer'
 import {validation} from './orgReg_reducer'
 // console.log(MemDetails)
 const rootReducer = combineReducers({
   MemDetails, OrgReg, usidIsAvailable, validation, 
-  orgAll:all, orgDetails:details, orgMembers:members, orgToken:token
+  orgAll:all, dashDetails:details, dashMembers:members, orgToken:token, isLoggedIn
 });
 
 export default rootReducer;
