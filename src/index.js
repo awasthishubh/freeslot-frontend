428ca70ae14cb67230a12a17a1c2762013d04dcb
awasthishubh <aw.shubh@gmail.com>
2018-07-13 04:47:42 +0530

    Added external materialize js file

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import Org_reg from './components/org_reg.js'
-import M from 'materialize-css'
+// import M from 'materialize-css'
 // import { providers } from 'redux'
 // store = createStore(rootReducer{})
 const store = createStore(rootReducer, applyMiddleware(thunk))
