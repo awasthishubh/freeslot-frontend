e14faf0d6e35a127fc4d21e814aebd1a85785005
awasthishubh <aw.shubh@gmail.com>
2018-07-13 12:03:07 +0530

    Added refresh button and scrolling

--- src/components/dashboardComp/fixedComp/index.js
+++ src/components/dashboardComp/fixedComp/index.js
@@ -8,10 +8,12 @@ import UpperRight from './dashUpperRight'
 import BottomLeft from './dashBottomLeft'
 import BottomRight from './dashBottomRight'
 
+// var style={
+//     position: 'fixed',top:64,left:300,right:0, bottom:0,padding:20, overflowY:'scroll',overflowX:'scroll', zIndex:104
+// }
 var style={
     position: 'absolute',top:64,left:300,right:0, bottom:0,padding:20, overflowY:'scroll', zIndex:104
 }
-
 export class dashboard extends Component{
     constructor(props){
         super(props)
@@ -27,10 +29,12 @@ export class dashboard extends Component{
                 <div>
                     <UpperLeft/>
                     <BottomLeft selected={this.props.dashSelected} select={this.props.updateSelected} members={this.props.dashMembers} requests={this.props.dashRequests}/>
-                    <UpperRight data={this.props.dashMembers} selected={this.props.dashModal} select={this.props.updateData}/>
+                    <UpperRight update={this.props.updateDashboardData} data={this.props.dashMembers} selected={this.props.dashModal} select={this.props.updateData}/>
                     <BottomRight/>
                     </div>
-                    <div className="bottomRightDash" style={style}>{this.props.children}</div>
+                    <div className="bottomRightDash" style={style}>
+                        <div className="innerDash" style={{height:"100%"}} >{this.props.children}</div>
+                    </div>
                     </div>
 
         )
