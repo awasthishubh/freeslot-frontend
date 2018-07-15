488a987338996af38fededaa9a918ec6abd03d3c
awasthishubh <aw.shubh@gmail.com>
2018-07-15 17:49:30 +0530

    Render graph on re rendering comp

--- src/components/chart/chart.js
+++ src/components/chart/chart.js
@@ -15,6 +15,50 @@ export default class extends Component {
         }
         else return "Morning"
     }
+    componentDidUpdate(){
+        console.log(this.chartDom.current.getContext('2d'))
+        var slots=this.props.slots
+        
+        var colors=['#00e676','#ff1744']
+        var data=[], colrs=[], labels=[]
+        var start=0, end=12
+        if(this.props.afternoon){start=12, end=24}
+        for(var i=start; i<end; i++){
+            data.push(1)
+            var k=i+1
+            if(i<10) i='0'+i
+            if(k<10) k='0'+k
+            labels.push(i+':00 - '+k+ ':00')
+            if(slots.indexOf(i-8)<0){
+                colrs.push(colors[0])
+            }
+            else{
+                colrs.push(colors[1])
+            }
+        }
+        
+        console.log(data, colrs)
+        
+        
+        var ctx = this.chartDom.current.getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: labels,
+                datasets: [{
+                    label: '# of Votes',
+                    data: data,
+                    backgroundColor: colrs,
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                legend: {
+                    display: false
+                }
+            }
+        });
+    }
 
     componentDidMount(){
         console.log(this.chartDom.current.getContext('2d'))
