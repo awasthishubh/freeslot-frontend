f3f338b9f9a49f496343dd208eeaa0666ce4c3ef
Shubham Awasthi <aw.shubh@gmail.com>
2019-11-17 19:52:14 +0530

    Updates

---
+++ precache-manifest.0e0e7c71506bd251a10aecd03d78ba78.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b109885547697fa1a2364e71be72e4c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fbbd0f9ea47e74f6415",
+    "url": "/static/js/2.dee640fd.chunk.js"
+  },
+  {
+    "revision": "1a4c7b83127ce32355e8",
+    "url": "/static/js/main.6018e185.chunk.js"
+  },
+  {
+    "revision": "8f3a47acdca985fcbf86",
+    "url": "/static/js/runtime-main.de12f889.js"
+  },
+  {
+    "revision": "558d800c8fb59a4b4bb56cd71f50af58",
+    "url": "/static/media/extention.558d800c.gif"
+  }
+]);
