709aa88c94fc6f65c65637d1e0d11d76425415dc
Shubham Awasthi <aw.shubh@gmail.com>
2019-12-15 15:36:09 +0530

    Disable member on mobile

--- src/components/home/regModal.js
+++ src/components/home/regModal.js
@@ -23,13 +23,20 @@ function getQueryVariable(variable) {
     return false
 }
 
+var mobilecheck = function() {
+    var check = false;
+    // eslint-disable-next-line
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+  };
+
 function DownloadExt(){
     return(
         <div>
             <h5 style={{margin:0}}>Download Extenstion</h5>
             Install a small extention to extract timetable from VTOP.
             <div style={{textAlign:'center', position:'relative'}}>
-                <img alt="download browser's extention" src={require('../../resources/extention.gif')} width="80%" useMap="#map"/>
+                <img alt="download browser's extention" src={require('../../static/extention.gif')} width="80%" useMap="#map"/>
                 <a target="_blank" rel="noopener noreferrer" style={{position:'absolute', top:0, height:'100%',left:0,width:'50%'}} href="http://awasthishubh.github.io/host-files/freeslot-1.1-an+fx.xpi"> </a>
                 <a target="_blank" rel="noopener noreferrer" style={{position:'absolute', top:0, height:'100%',left:'50%',width:'50%'}} href="https://chrome.google.com/webstore/detail/freeslot/lfnajapcpchmnpnjfenkneiphhmhhafa/"> </a>
             </div>
@@ -96,9 +103,11 @@ class Submit_card extends Component {
                     <h4>Image file not recognised</h4>
                     Constraints for uploaded screnshot:
                     <ul className="browser-default" style={{listStyleType: 'disc'}}>
+                        <li>{/* eslint-disable-next-line */}
+                            <a onClick={this.getExt.bind(this)} style={{cursor:'pointer'}}>Get it from vtop.</a>
+                        </li>
                         <li> Image must be png file</li>
                         <li> Image must be horizontally complete, i.e. must show all 13 slots in a day for all the included days</li>
-                        <li> Image can be cropped vertically to include only those rows where there is a filled slot.</li>
                         <li> Every individual cell included in the image should be present completely. The cropping should not overlap text inside the slot boxes</li>
                     </ul>
                 </div>
@@ -272,6 +281,21 @@ class Submit_card extends Component {
     }
     
     render(){
+        if(mobilecheck()){
+            return(
+                <Modal 
+                    footerFixed={false}
+                    style={{top:'5%', maxHeight:'90%'}} 
+                    modalDom={this.modalDom}
+                    footer={[]}
+                >
+                    <h4>Mobile Browser detected.</h4>
+                    <p>
+                        This feature is available only on desktop browser.
+                    </p>
+                </Modal>
+                )
+        }
         if(this.state.modal==='RESPONSE')
             return(
                 <Modal 
@@ -344,7 +368,7 @@ function Input(props){
 
 function InputForm(){
     return(
-        <div className="row">
+        <div className="row" style={{marginBottom:0}}>
         <Input id='mem_name' label='Name' 
             value={this.props.MemDetails.name} 
             onChange={(e)=>this.onChange(e,'UPDATE_NAME',1)}
@@ -387,7 +411,7 @@ function File(props){
     return(
         <fieldset>
             <legend><b>TimeTable</b></legend>
-            <div className="row" style={{position:'relative'}}>
+            <div className="row" style={{marginBottom:0, position:'relative'}}>
                 <div className="file-field input-field col s12 m6">
                     <div className="btn">
                         <span>Upload</span>
@@ -403,7 +427,7 @@ function File(props){
                 <div className="col s12 hide-on-med-and-up" style={{textAlign:"center"}}>OR</div>
                 <div className="input-field  col m5 s12" style={{textAlign:"center",fontSize:20,padding:10}}>
                     {/* eslint-disable-next-line */}
-                    <a onClick={props.extClick} style={{cursor:'pointer'}}>Get it from vtop.</a>
+                    <a onClick={props.extClick} style={{cursor:'pointer'}}>Get it from vtop.</a><br/>(Recommended)
                 </div>
             </div>
 
